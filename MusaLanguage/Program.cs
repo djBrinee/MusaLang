0db38daa93d78ec0532bccbd03dd907caa5ee507
zolanovich <avilajuan0301@gmail.com>
2022-12-03 14:56:01 -0400

    Manejo de errores

--- MusaLanguage/Program.cs
+++ MusaLanguage/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MusaLanguage.Clases;
 using System.IO;
+using Antlr4.Runtime.Misc;
 
 namespace MusaLanguage
 {
@@ -14,15 +15,61 @@ namespace MusaLanguage
         static void Main(string[] args)
         {
             Visitor visitor = new Visitor();
+            ImmediateErrorListener errListener = ImmediateErrorListener.Instance;
+
             var input = CharStreams.fromPath("input.musa");
             var lexer = new musaLexer(input);
             var tokenStream = new CommonTokenStream(lexer);
             var parser = new musaParser(tokenStream);
-            var tree = parser.musa(); //Ejecucion de regla inicial
-            string fuente = visitor.Visit(tree);
-            File.WriteAllText("traduccion.cs", fuente);
-            Console.WriteLine("Traduccion generada, presione ENTER para cerrar la consola...");
-            Console.ReadLine();
+            parser.RemoveErrorListeners();
+            parser.AddErrorListener(errListener);
+            string fuente = "";
+            bool ok = false;
+            do
+            {
+                try
+                {
+                    var tree = parser.musa(); //Ejecucion de regla inicial
+                    fuente = visitor.Visit(tree);
+                    File.WriteAllText("traduccion.cs", fuente);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Traduccion generada, presione ENTER para cerrar la consola...");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    ok = true;
+                }
+                catch (ParseCanceledException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                    Console.WriteLine("Debe seguir las reglas del lenguaje musa! Presione ENTER para cerrar la consola y arregle sus errores");
+                    fuente = e.Message;
+                    File.WriteAllText("traduccion.cs", fuente);
+                    Console.ReadLine();
+                    break;
+                }
+            } while (!ok);
+            
+            
         }
     }
+
+    internal class ImmediateErrorListener: BaseErrorListener
+    {
+        
+            static Lazy<ImmediateErrorListener> instance = new Lazy<ImmediateErrorListener>(() => new ImmediateErrorListener());
+
+            public static ImmediateErrorListener Instance
+            {
+                get { return instance.Value; }
+            }
+
+            public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+            {
+                throw new Antlr4.Runtime.Misc.ParseCanceledException($"Error en la línea {line} columna {charPositionInLine}: {msg}");
+            }
+        
+    }
+
 }
